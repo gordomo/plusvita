226adc493b77cf1920ea195537b809a33bbd4acc
gordomo <morimartin@gmail.com>
2022-08-02 16:53:52 -0300

    pv-13 - problema de time out

--- src/Controller/ImprimirController.php
+++ src/Controller/ImprimirController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Repository\ClienteRepository;
 use Knp\Snappy\Pdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\File;
@@ -103,7 +104,7 @@ HTML;
             'page-size' => 'A4',
             'margin-bottom' => '5',
             'no-custom-header-propagation' => true,
-            'encoding' => 'utf-8'
+            'encoding' => 'UTF-8'
         ];
 
         $knpSnappyPdf->setTimeout(60000000000)->generateFromHtml($html, '/var/www/html/var/cache/dev/snappy/bill-123.pdf', $options, true);
@@ -111,13 +112,133 @@ HTML;
         $error = false;
 
         if(file_exists('/var/www/html/var/cache/dev/snappy/bill-123.pdf')) {
-            copy('/var/www/html/var/cache/dev/snappy/bill-123.pdf', '/var/www/symfony/public/uploads/snappy/historia' . date('d-m-y-h-i-s') . '.pdf');
+            $fileName = 'historia' . date('d-m-y-h-i-s') . '.pdf';
+            copy('/var/www/html/var/cache/dev/snappy/bill-123.pdf', '/var/www/html/public/uploads/snappy/'.$fileName);
             unlink('/var/www/html/var/cache/dev/snappy/bill-123.pdf');
         } else {
             $error = true;
         }
 
-        return new JsonResponse(['url' => '/uploads/snappy/bill-123.pdf', 'error' => $error]);
+        return new JsonResponse(['url' => '/uploads/snappy/'.$fileName, 'error' => $error]);
+
+    }
+
+    /**
+     * @Route("/pdf2", name="imprimir_pdf2", methods={"POST", "GET"})
+     */
+    public function pdf2(Request $request, Pdf $knpSnappyPdf, ClienteRepository $clienteRepository): Response
+    {
+        $seccionesSelected = $request->get('seccionesSelected');
+        $clientId = $request->get('clientId');
+        $cliente = $clienteRepository->find($clientId);
+
+        $ingresoHtml = '';
+
+        foreach ($seccionesSelected as $seccion) {
+            switch ($seccion) {
+                case 'historia-al-ingreso':
+                    $ingresoHtml = $this->getIngresoHtml($cliente);
+                    break;
+                case 'notas':
+                    break;
+                case 'notas-en-turnos':
+                    break;
+                case 'evoluciones-imprimir':
+                    break;
+                case 'novedades':
+                    break;
+                case 'epicrisis-al-alta':
+                    break;
+            }
+        }
+
+        $options = [
+            'page-size' => 'A4',
+            'margin-bottom' => '5',
+            'no-custom-header-propagation' => true,
+            'encoding' => 'UTF-8'
+        ];
+
+        $fileName = 'historia' . date('d-m-y-h-i-s') . '.pdf';
+
+        $knpSnappyPdf->generateFromHtml($ingresoHtml, '/var/www/html/public/uploads/snappy/'.$fileName, $options, true);
+
+        return new JsonResponse('/var/www/html/public/uploads/snappy/'.$fileName);
+    }
+
+    private function getIngresoHtml($cliente) {
+        $ingreso = $cliente->getHistoriaIngreso();
+        $ingresoHtml = '';
+        if (!empty($ingreso)) {
+            if (!empty($ingreso->getExamenesComplementeriosFiles())) {
+                foreach ($ingreso->getExamenesComplementeriosFiles() as $examenes) {
+                    $url = 'uploads/adjuntos/pacientes/' . $cliente->getId() . "/complementerios/" . $examenes;
+                    $link = $this->generateUrl('download_pdf_adjunto', ['path'=> $url, 'nombre'=> $examenes]);
+                    $linksComplementarios = "<a href='$link'>$examenes</a><br>";
+                }
+            } else {
+                $linksComplementarios = 'No tiene';
+            }
+
+            $antecedentesTexto = $ingreso->getAntecedentesTexto();
+            $enfermedadActual = $ingreso->getEnfermedadActual();
+            $fisico = $ingreso->getExamenFisicoAlIngreso();
+            $desc = $ingreso->getExamenComplementarioDesc();
+            $indicaciones = $ingreso->getIndicaciones();
+
+
+            $ingresoHtml = <<<HTML
+                <tr>
+                    <th>Antecedentes:</th>
+                </tr>
+                <tr>
+                    <td>$antecedentesTexto</td>
+                </tr>
+                <tr>
+                    <th>Enfermedad Actual:</th>
+                </tr>
+                <tr>
+                    <td>$enfermedadActual</td>
+                </tr>
+                <tr>
+                    <th>Exámen Físico al ingreso:</th>
+                </tr>
+                <tr>
+                    <td>$fisico</td>
+                </tr>
+                <tr>
+                    <th>Exámen Complementario:</th>
+                </tr>
+                <tr>
+                    <td>$desc</td>
+                </tr>
+                <tr>
+                    <th>Indicaciones:</th>
+                </tr>
+                <tr>
+                    <td>$indicaciones</td>
+                </tr>
+                <tr>
+                    <th>Adjuntos:</th>
+                    <td>$linksComplementarios</td>
+                </tr>
+HTML;
+
+        }
+        
+        
+        $return = <<<HTML
+<div class="row">
+        <div class="col-12 historia-al-ingreso" >
+            <h4 style="border-top: 1px solid; padding-top: 10px">Ingreso</h4>
+            <table class="table table-responsive table-striped">
+                $ingresoHtml
+            </table>
+        </div>
+HTML;
+
+
+        return $return;
 
     }
 
