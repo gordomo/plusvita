07bf63dcf25cff2a429a8262e214b9db72a903f6
Martin Mori <morimartin@gmail.com[201~>
2024-06-04 10:04:57 -0300

    dev - vuelvo a agregar validación para no evolucionar fuera de fecha

--- src/Controller/EvolucionController.php
+++ src/Controller/EvolucionController.php
@@ -129,8 +129,7 @@ class EvolucionController extends AbstractController
                 $hoy = new \DateTime();
                 
                 if ($evolucion->getFecha()->diff($hoy)->days > 0 && !$puedenEditarEvoluciones) {
-                    //die('la fecha de la evolución es anterior al día de la fecha, no se puede evolucionar');
-                    //hay que volver a poner esto el lunes
+                    die('la fecha de la evolución es anterior al día de la fecha, no se puede evolucionar');
                 }
                 
                 foreach($adjuntos as $adjunto) {
